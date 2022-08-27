245b6d938155078ed39e2de77a9a4f0c4c6c72b4
Cristian Beltran <cristian.beltran.paco@gmail.com>
2022-08-27 17:50:01 -0400

    Correcion

--- app/urls.py
+++ app/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +28,5 @@ urlpatterns = [
     path('logout',views.logout_view,name='logout'),
 
     path('amenazas/',include('amenazas.urls'))
-]
+]+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+
